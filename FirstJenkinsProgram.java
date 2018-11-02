74de39e571eb0ed1df48d5c158c6db71d212b0ff
Rohilla, Pradeep K <pkumar@optum.com>
2018-11-02 20:29:02 +0530

    checking in first java prg

---
+++ FirstJenkinsProgram.java
@@ -0,0 +1,11 @@
+
+public class FirstJenkinsProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my first Jenkins Program");
+
+	}
+
+}
